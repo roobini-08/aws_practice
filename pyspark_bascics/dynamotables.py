5d0ef09c218570cf08bdcfc49df26ab19703a37e
Roobini <rooshivv@gmail.com>
2024-11-05 20:57:35 +0530

    commit

---
+++ pyspark_bascics/dynamotables.py
@@ -0,0 +1,14 @@
+#Write a Python program using Boto3 to list all DynamoDB tables in your AWS account and print their names.
+
+import boto3
+ 
+dynamodb_client = boto3.client('dynamodb', region_name='us-east-1')  
+ 
+try:
+    response = dynamodb_client.list_tables()
+    table_names = response['TableNames']
+    print("DynamoDB Tables:")
+    for table_name in table_names:
+        print(table_name)
+except Exception as e:
+    print("Error listing tables:", e)
